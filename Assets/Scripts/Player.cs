3cf647913a8d6408ccc79bb33b6713aecaee9235
pocket-steve <steven@pocketworlds.com>
2023-09-14 15:50:10 -0600

    Path nicely and give a better speed to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public NavGrid Grid;
+    public Vector3 Destination;
+    public float Speed = 10.0f;
     
     void Start()
     {
-        Grid.Initialize();        
+        Destination = transform.position;
+        Grid.Initialize();
     }
 
     void Update()
@@ -16,8 +19,17 @@ public class Player : MonoBehaviour
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hitInfo))
             {
-                transform.position = Grid.GetPath(transform.position, hitInfo.point)[^1].Position;
+                Destination = Grid.GetPath(transform.position, hitInfo.point)[^1].Position;
             }
+        }
+
+        if (transform.position != Destination)
+        {
+            var maxDistance = Speed * Time.deltaTime;
+            var vectorToDestination = Destination - transform.position;
+            var moveDistance = Mathf.Min(vectorToDestination.magnitude, maxDistance);
+            
+            transform.position += vectorToDestination.normalized * moveDistance;
         }
     }
 }
